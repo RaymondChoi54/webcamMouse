ac921a89d15900eac83e2cebfe772c5b79e3e525
RaymondChoi54 <Concept_ray@hotmail.com>
2017-02-09 16:27:30 -0500

    Updated the class

--- webcam_mouse/MouseControl.py
+++ webcam_mouse/MouseControl.py
@@ -1,31 +1,34 @@
 import pyautogui
 
-class MouseControl(x, y, center_x, center_y):
-	self.x = x
-	self.y = y
-	self.center_x = center_x
-	self.center_y = center_y
 
-class move_mouse(x, y):
-	pyautogui.moveTo(x, y, duration=p)
+class MouseControl:
 
-class auto_res():
-	x = pyautogui.size()[0]
-	y = pyautogui.size()[1]
+	class __init__(self, x, y, center_x, center_y):
+		self.x = x
+		self.y = y
+		self.center_x = center_x
+		self.center_y = center_y
 
-class mouse_loc():
-	pyautogui.position()
+	def move_mouse(x, y):
+		pyautogui.moveTo(x, y, duration=0)
 
-class click(type):
-	if(type == "left"):
-		pyautogui.click()
-	elif(type == "right"):
-		pyautogui.rightClick()
-	elif(type == "middle"):
-		pyautogui.middleClick()
-	elif(type == "double"):
-		pyautogui.doubleClick()
-	elif(type == "hold"):
-		pyautogui.mouseDown()
-	elif(type == "release"):
-		pyautogui.mouseUp()
+	def auto_res():
+		x = pyautogui.size()[0]
+		y = pyautogui.size()[1]
+
+	def mouse_loc():
+		pyautogui.position()
+
+	def click(type):
+		if(type == "left"):
+			pyautogui.click()
+		elif(type == "right"):
+			pyautogui.rightClick()
+		elif(type == "middle"):
+			pyautogui.middleClick()
+		elif(type == "double"):
+			pyautogui.doubleClick()
+		elif(type == "hold"):
+			pyautogui.mouseDown()
+		elif(type == "release"):
+			pyautogui.mouseUp()
