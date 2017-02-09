82411e1f6fac1f703f8da8816c9ce1b28afbde06
RaymondChoi54 <Concept_ray@hotmail.com>
2017-02-09 16:34:13 -0500

    Added smart mouse move

--- webcam_mouse/MouseControl.py
+++ webcam_mouse/MouseControl.py
@@ -1,20 +1,22 @@
 import pyautogui
 
-
 class MouseControl:
 
-	class __init__(self, x, y, center_x, center_y):
-		self.x = x
-		self.y = y
+	class __init__(self, screen_x, screen_y, center_x, center_y):
+		self.screen_x = screen_x
+		self.screen_y = screen_y
 		self.center_x = center_x
 		self.center_y = center_y
+		
+	def smart_mouse_move(self, x, y):
+		pyautogui.moveTo(x, y, duration=0)
 
 	def move_mouse(x, y):
 		pyautogui.moveTo(x, y, duration=0)
 
-	def auto_res():
-		x = pyautogui.size()[0]
-		y = pyautogui.size()[1]
+	def auto_res(self):
+		self.screen_x = pyautogui.size()[0]
+		self.screen_y = pyautogui.size()[1]
 
 	def mouse_loc():
 		pyautogui.position()
