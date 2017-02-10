4585dbfc452c35f0accca67e0b73a13ae293b672
kevinwang411 <kevinwang411@gmail.com>
2017-02-10 01:06:24 -0500

    new button opens up a new tab js

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-document.getElementById("button_1").addEventListener("click", function(){
-  var ele = document.createElement("P");
-  ele.innerHTML="Hello World!";
-  document.getElementById("body").appendChild(ele);
 
-},false);
+	document.getElementById("button_1").addEventListener("click", function(){
+  		var ele = document.createElement("P");
+  		ele.innerHTML="Hello World!";
+  		document.getElementById("body").appendChild(ele);
+	},false);
+
+	document.getElementById("button_2").addEventListener("click", function(){
+  		chrome.tabs.create({'url':'chrome://newtab', 'selected': true});
+	},false);
+
 },false);
