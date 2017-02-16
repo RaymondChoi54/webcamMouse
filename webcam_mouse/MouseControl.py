e71d45380114e91df7969da581717e5bdb7233c9
RaymondChoi54 <Concept_ray@hotmail.com>
2017-02-16 00:43:17 -0500

    Fixed an bug

--- webcam_mouse/MouseControl.py
+++ webcam_mouse/MouseControl.py
@@ -1,8 +1,8 @@
 import pyautogui
 
-class MouseControl:
+class MouseControl(object):
 
-	class __init__(self, screen_x, screen_y, center_x, center_y, circ_raid, x_sensitivity, y_sensitivity):
+	def __init__(self, screen_x, screen_y, center_x, center_y, circ_raid, x_sensitivity, y_sensitivity):
 		self.screen_x = screen_x
 		self.screen_y = screen_y
 		self.center_x = center_x
@@ -12,24 +12,19 @@ class MouseControl:
 		self.y_sensitivity = y_sensitivity
 		
 	def smart_mouse_move(self, x, y):
-		
-		int x_move = 0
-		int y_move = 0
-		
 		current_x, current_y = pyautogui.position()
 		if(x > self.center_x + circ_raid):
-			x_move = - x_sensitivity
+			pyautogui.moveTo(current_x + x_sensitivity, current_y, duration=0)
 		elif(x < self.center_x - circ_raid):
-			x_move = x_sensitivity
+			pyautogui.moveTo(current_x - x_sensitivity, current_y, duration=0)
 			
+		current_x, current_y = pyautogui.position()
 		if(y > self.center_y + circ_raid):
-			y_move = y_sensitivity
+			pyautogui.moveTo(current_x, current_y + y_sensitivity, duration=0)
 		elif(y < self.center_y - circ_raid):
-			y_move = -y_sensitivity
-			
-		pyautogui.moveTo(current_x + x_move, current_y + y_move, duration=0)
+			pyautogui.moveTo(current_x, current_y - y_sensitivity, duration=0)
 
-	def move_mouse(x, y):
+	def move_mouse(self, x, y):
 		pyautogui.moveTo(x, y, duration=0)
 		
 	def recalibrate(self, x, y):
@@ -40,10 +35,10 @@ class MouseControl:
 		self.screen_x = pyautogui.size()[0]
 		self.screen_y = pyautogui.size()[1]
 
-	def mouse_loc():
+	def mouse_loc(self):
 		pyautogui.position()
 
-	def click(type):
+	def click(self, type):
 		if(type == "left"):
 			pyautogui.click()
 		elif(type == "right"):
@@ -56,3 +51,4 @@ class MouseControl:
 			pyautogui.mouseDown()
 		elif(type == "release"):
 			pyautogui.mouseUp()
+
