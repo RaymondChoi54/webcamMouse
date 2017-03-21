560f24961f3c486ced2c81c3ba3df83ee8454870
Timothy Lock <lock.k.timothy@gmail.com>
2017-03-21 13:33:56 -0400

    Code Cleanup / Sensitivity adjust basic framework

--- webcam_mouse/faceTrack.py
+++ webcam_mouse/faceTrack.py
@@ -5,27 +5,25 @@ from MouseControl import MouseControl
 import threading
 import time
 import pygame
-
+import ctypes
 
 # Create mousecontrol ocject for controlling use
 mouseControl = None
 
 # Import Haar Classifiers
 faceCascade = cv2.CascadeClassifier(sys.argv[1])
-eyesCascade = cv2.CascadeClassifier(sys.argv[2])
-leftEyeOpen = cv2.CascadeClassifier(sys.argv[3])
-rightEyeOpen = cv2.CascadeClassifier(sys.argv[4])
 
 # Set up
 video_capture = cv2.VideoCapture(0)
-# Try to get max resolution
-
 mouseInstructionImg = cv2.imread('assets\pictures\moveFace.png')
 clickInstructionImg = cv2.imread('assets\pictures\holdClick.png')
 sensitivityImg = cv2.imread('assets\pictures\sensitivity.png')
 nx, ny = 0, 0;
 maxX, maxY, minX, minY = None, None, None, None
-centerX, centerY = None, None
+centerX, centerY = 300, 300
+
+# Before We Begin
+ctypes.windll.user32.MessageBoxA(0, "Please make sure that the webcam is facing the subject and that the volume is turned up", "Before we begin", 0)
 
 # Begin Program
 cv2.namedWindow("calibrate", cv2.WND_PROP_FULLSCREEN)
@@ -56,11 +54,34 @@ while True:
 		flags=cv2.CASCADE_SCALE_IMAGE
 	)
 
+	# Calibration always on screen text
+	if ((time.time() - startTime) > 6 and (time.time() - startTime) < 13):
+		cv2.putText(frame,"Stage 1 Calibration -- Seconds Left: " + str((startTime + 13 - time.time())), (10,400), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
+	elif ((time.time() - startTime) > 12 and (time.time() - startTime) < 36):
+		cv2.putText(frame,"Stage 2 Calibration -- Seconds Left: " + str((startTime + 36 - time.time())), (10,400), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
+		if ((time.time() - startTime) > 12 and (time.time() - startTime) < 27):
+			frame[0:0+mouseInstructionImg.shape[0], 0:0+mouseInstructionImg.shape[1]] = mouseInstructionImg
+		else:
+			frame[0:0+clickInstructionImg.shape[0], 0:0+clickInstructionImg.shape[1]] = clickInstructionImg
+		cv2.rectangle(frame, (centerX, centerY), (centerX + 1, centerY + 1), (255, 255, 255), 1) # Draw a center dot
+		cv2.rectangle(frame, (minX, minY), (maxX, maxY), (255, 0, 0), 1) # Draw a rectangle around the calibration
+	elif ((time.time() - startTime) > 27 and (time.time() - startTime) < 48):
+		cv2.putText(frame,"Stage 3 Calibration -- Seconds Left: " + str((startTime + 48 - time.time())), (10,400), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
+		frame[0:0+sensitivityImg.shape[0], 0:0+sensitivityImg.shape[1]] = sensitivityImg
+		print(centerX)
+		cv2.rectangle(frame, (centerX, centerY), (centerX + 1, centerY + 1), (255, 255, 255), 1) # Draw a center dot
+		cv2.line(frame, ((centerX), 0), ((centerX), frameWidth), (255,255,255), 2)
+		cv2.line(frame, (0, (centerY)), (frameHeight, (centerY)), (255,255,255), 2)
+		cv2.putText(frame,"1", (centerX-95,centerY-30), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 3)
+		cv2.putText(frame,"2", (centerX+15,centerY-30), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 3)
+		cv2.putText(frame,"4", (centerX+15,centerY+130), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 3)
+		cv2.putText(frame,"3", (centerX-100,centerY+130), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 3)
+
+	cv2.putText(frame,"press Q to quit", (0,20), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0))
+
 	for (x, y, w, h) in faces:
 		# Calibration
-		if ((time.time() - startTime) > 6 and (time.time() - startTime) < 13):
-			cv2.putText(frame,"Stage 1 Calibration -- Seconds Left: " + str((startTime + 13 - time.time())), (10,400), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
-			
+		if ((time.time() - startTime) > 6 and (time.time() - startTime) < 13):		
 			# Obtain center
 			centerX = x + (w/2)
 			centerY = y + (h/2)
@@ -71,13 +92,7 @@ while True:
 				minX = centerX
 				maxY = centerY
 				minY = centerY
-		elif ((time.time() - startTime) > 12 and (time.time() - startTime) < 37):
-			cv2.putText(frame,"Stage 2 Calibration -- Seconds Left: " + str((startTime + 37 - time.time())), (10,400), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255))
-			if ((time.time() - startTime) > 12 and (time.time() - startTime) < 27):
-				frame[0:0+mouseInstructionImg.shape[0], 0:0+mouseInstructionImg.shape[1]] = mouseInstructionImg
-			else:
-				frame[0:0+clickInstructionImg.shape[0], 0:0+clickInstructionImg.shape[1]] = clickInstructionImg
-
+		elif ((time.time() - startTime) > 12 and (time.time() - startTime) < 36):
 			# Keep getting max and min
 			if (x + (w/2) < minX):
 				minX = x + (w/2)
@@ -91,8 +106,6 @@ while True:
 			# Obtain center
 			centerX = (maxX + minX)/2
 			centerY = (maxY + minY)/2
-			cv2.rectangle(frame, (centerX, centerY), (centerX + 1, centerY + 1), (255, 255, 255), 1) # Draw a center dot
-			cv2.rectangle(frame, (minX, minY), (maxX, maxY), (255, 0, 0), 1) # Draw a rectangle around the calibration
 
 		# Draw crosshair on person
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1) # Draw a rectangle around the faces	
@@ -103,20 +116,17 @@ while True:
 		nx = (x + w / 2)
 		ny = (y + h / 2)
 
-		#cv2.putText(frame,"Track Coordinates: (" + str(nx) + "," + str(ny) + ")", (10,50), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0))
-		cv2.putText(frame,"press Q to quit", (0,20), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0))
-
 		# Send coordinates over to mouse control only if not in calibration mode
-		if ((time.time() - startTime) > 37):
+		if ((time.time() - startTime) > 48):
 			# Create Mouse Control if not created
 			if not mouseControl:
-				mouseControl = MouseControl(frame.shape[1], frame.shape[0], centerX, centerY, min((maxX - centerX), (maxY - centerY), (centerY - minY), (centerX - minX)), 50, 50)
+				mouseControl = MouseControl(frame.shape[1], frame.shape[0], centerX, centerY, min((maxX - centerX), (maxY - centerY), (centerY - minY), (centerX - minX)), 0.4, 0.4)
 			
 			thr = threading.Thread(target=mouseControl.smart_mouse_move, args=(nx, ny), kwargs={})
 			thr.start()
 
 	# Display the resulting frame
-	if ((time.time() - startTime) > 0 and (time.time() - startTime) < 37):
+	if ((time.time() - startTime) > 0 and (time.time() - startTime) < 48):
 		cv2.imshow("calibrate", frame)
 	else:
 		cv2.circle(frame, (centerX, centerY), min((maxX - centerX), (maxY - centerY), (centerY - minY), (centerX - minX)), (255, 255, 255), 1) # Draw curcle around area
