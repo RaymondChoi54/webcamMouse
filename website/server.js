f8d5125f531b699811fb28f8fc7c79bcd57261c3
Rohan Bhatia <rohan1413@gmail.com>
2017-03-24 12:01:53 -0400

    added in email functionality

--- website/server.js
+++ website/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//var nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer');
 
 var app = express();
 //app.use(express.static(__dirname + '/assets'));
@@ -27,7 +27,37 @@ app.get('/download', function(req, res) {
 
 //submit feedback
 app.post('/submit', function(req, res) {
+	
+	console.log("POST /submit")
 
+	email = req.body.email
+
+	fullText = "Hello " + req.body.firstname + ",\nThank you for your message. We greatly value the opinion of our users. We'll review the feedback and get in touch!\nThanks,\nThe LimbsFree Team"
+
+	transporter = nodemailer.createTransport({
+    	service: 'gmail',
+    	auth: {
+        	user: 'limbsfree@gmail.com',
+        	pass: 'helloworld'
+    	}
+	});
+
+	mailOptions = {
+    from: '"LimbsFree Team" <limbsfree@gmail.com>', // sender address
+    to: email, // list of receivers
+    subject: 'Thank you for your comments', // Subject line
+    text: fullText, // plain text body
+	};
+
+	transporter.sendMail(mailOptions, function(err, info) {
+    	if (error) {
+        	return console.log(error);
+    	}
+    	console.log('Message %s sent: %s', info.messageId, info.response);
+	});
+
+
+	res.sendfile('homepage.html')
 });
 
 app.listen(process.env.PORT || 3000);
